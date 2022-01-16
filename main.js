b8d15dd007313bbb354a4f44a810c235a5ed4fe2
Alvin-He <shinypeach36@gmail.com>
2022-01-15 22:00:59 -0800

    trying to make a js amination

---
+++ main.js
@@ -0,0 +1,22 @@
+
+let print = console.log;
+
+$(document).ready(function () {
+
+    let content = $('#content')
+
+    let inputString = "High School Freshmen"
+
+    // checking for elements 
+    if (content.length) {
+        let p = $("<p></p>")
+
+        for (let i = 0; i < array.length; i++) {
+            const element = array[i];
+            
+        }
+    }
+
+    print(content.children())
+
+});
