da5e9a9b253c4671de8143773ecc599ad1c90c35
Alvin-He <shinypeach36@gmail.com>
2022-01-16 22:19:52 -0800

    finished #content 's animations

--- main.js
+++ main.js
@@ -1,22 +1,43 @@
 
+// section-util
 let print = console.log;
 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+// main
 $(document).ready(function () {
 
-    let content = $('#content')
+    let content = $('#content');
 
-    let inputString = "High School Freshmen"
+    //let inputString = "High School Freshmen"
 
-    // checking for elements 
+    // checking for elements
     if (content.length) {
-        let p = $("<p></p>")
+        (async () => { 
 
-        for (let i = 0; i < array.length; i++) {
-            const element = array[i];
-            
-        }
+            let contentObjectArray = content.find('p');
+
+            for (let i = 0; i < contentObjectArray.length; i++) {
+                const currentElement = contentObjectArray.eq(i);
+                const stringToAnimate = currentElement.html();
+                currentElement.html("");
+
+                for (let i = 0; i < stringToAnimate.length; i++) {
+                    const currentChar = stringToAnimate[i]; // convert into char stream
+
+                    currentElement.html(currentElement.html() + currentChar);
+                    // print('updated ' + currentChar);
+                    await sleep(10);
+                }
+            }
+        })();
+        // _call(); 
     }
 
-    print(content.children())
+    
+
+    // print(content.children())
 
 });
